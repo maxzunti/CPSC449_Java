bab66cbee7a1b1bfa65f71771146dbd046983de9
Steven Vi <stevenvi@outlook.com>
2016-03-12 02:58:39 -0700

    The compile steps are in the README.md, also added some more to the commandLnInterpreter, not complete or really readable yet

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CPSC449_Java
+# Steps to run the program
+# javac methods/*.java
+# jar cfm methods.jar Manifest.txt methods/*
+# java -jar methods.jar 
--- methods/CommandLnInterpreter.java
+++ methods/CommandLnInterpreter.java
@@ -1,7 +1,18 @@
 package methods;
 
-public class CommandLnInterpreter{
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.ClassNotFoundException;
+import java.lang.reflect.*;
 
+
+
+public class CommandLnInterpreter{
 
   // takes the command line args and execute the correct results
   public CommandLnInterpreter (String [] commandLnArgs) {
@@ -10,7 +21,6 @@ public class CommandLnInterpreter{
 
 //Given a list of all the command args, execute the apporpriate action
   public void interpretArgs (String [] commandLnArgs){
-
 //If there is no command line arguments, then print Synopsis
     if (commandLnArgs.length == 0){
       printSynopsis();
@@ -23,30 +33,73 @@ public class CommandLnInterpreter{
       System.out.println ("methods in <class-name>, and executes them, printing the result to sysout.");
       System.exit(0);
     }
-//If there is a verbrose argument first followed by a valid jar and class name, then continue
-// TODO: check Valid jar and class
+    //If there is a verbrose argument first followed by a valid jar and class name, then continue
+    // TODO: check Valid jar and class
     else if (validVerboseArg(commandLnArgs[0]) && commandLnArgs.length == 3){
+      getFromJar(commandLnArgs[1], commandLnArgs[2]);
 
     }
-//If there is a verbroseHelper argument first followed by a valid jar and class name, then continue
-// TODO: check Valid jar and class
+    //If there is a verbroseHelper argument first followed by a valid jar and class name, then continue
+    // TODO: check Valid jar and class
     else if (validVerbHelpArg(commandLnArgs[0]) && commandLnArgs.length == 3){
       printSynopsis();
       System.out.println ("\nThis program interprets commands of the format '(<method> {arg}*)' on the command line, finds corresponding");
       System.out.println ("methods in <class-name>, and executes them, printing the result to sysout.");
     }
-//If there is a valid jar and class name, then continue
-// TODO: check Valid jar and class
+    //If there is a valid jar and class name, then continue
+  // TODO: check Valid jar and class
     else if (commandLnArgs.length == 2){
 
     }
-//if none of the above the print error message and quit
-//TODO: print correct error message
+    //if none of the above the print error message and quit
+  //TODO: print correct error message
     else{
       System.out.println ("Bad args");
       System.exit(0);
     }
   }
+
+  public void getFromJar(String jarName, String className){
+    try{
+      File f = new File(jarName);
+      URL[] urls = new URL[1];
+      urls[0] = f.toURI().toURL();
+      URLClassLoader cl = URLClassLoader.newInstance(urls);
+      Class<?> c = Class.forName(className, true, cl);
+      //Class c = cl.loadClass(commandLnArgs[2]);
+
+      String testclassName = c.getName();
+      System.out.println("Class Name is: " + testclassName);
+
+      Constructor<?> classConstructor = c.getConstructor(new Class<?>[0]);
+      Object classObj = classConstructor.newInstance();
+      c.cast(classObj);
+
+      Method oneMethod = c.getMethod("add", new Class[] { Integer.TYPE, Integer.TYPE});
+      System.out.println("Method is: " + oneMethod);
+
+      System.out.println("DOES METHOD WORK " + oneMethod.invoke(classObj, 4, 2));
+
+    }catch(MalformedURLException ex){
+      System.out.println ("AAAAHHHHH");
+      System.exit(0);
+    }catch(ClassNotFoundException ex){
+      System.out.println ("AAAAHHHHH2");
+      System.exit(0);
+    } /*catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } */catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } /*catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } */catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+      }
 
 //check if string is verboseHelper arg
   public Boolean validVerbHelpArg(String helpStr){
