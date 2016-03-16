03b48cb49824a07856d44336cc3b504be8931a8b
pgchowwa <pgchowwa@ucalgary.ca>
2016-03-16 17:43:21 -0600

    add get func method to FunctionsFromFile

--- methods/MainLoop.java
+++ methods/MainLoop.java
@@ -2,6 +2,7 @@ package methods;
 
 // Imports
 import java.util.Scanner;
+import java.lang.reflect.*;
 
 public class MainLoop{
 
@@ -12,20 +13,27 @@ public class MainLoop{
     CommandLnInterpreter comLn = new CommandLnInterpreter(args);
     FunctionsFromFile functions = comLn.getFunctionFromFile();
 
-    ////*******Testing executeMethod DOES NOT WORK
-    //Integer test1 = new Integer(1);
-    //int test1 = 30;
-    //Integer test2 = new Integer(2);
-    //Object[] array = {test1};
-    //System.out.println(test1.getClass());
-    //functions.executeMethod("inc", array);
+
+
+    /*******Testing get Method
+    ParseNode.rType[] test = new ParseNode.rType[2];
+    test[0] = ParseNode.rType.FLOAT;
+    test[1] = ParseNode.rType.FLOAT;
+
+    Method testMethod = functions.getFuncMethod("testsds", test);
+    System.out.println(testMethod.getName());
+    for (Class<?> para : testMethod.getParameterTypes())
+      System.out.println(para);
+      */
+
+
     String expr;
     Scanner reader = new Scanner(System.in);
     do{
       System.out.print("> ");
       expr = reader.next();
       // parseTree.doEverything(expr);
-    } while(!expr.equals("q") && !expr.equals("quit"));
+    } while(!expr.equalsIgnoreCase("q") && !expr.equalsIgnoreCase("quit"));
 
     System.out.println ( "This is where the main loop will be" );
   }
