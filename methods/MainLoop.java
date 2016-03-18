09a8903d6aa93cb24964026ed73976aee8e8a6a9
mrzunti <mrzunti@zone33-ed.cs.ucalgary.ca>
2016-03-18 16:05:56 -0600

    some final cleanup

--- methods/MainLoop.java
+++ methods/MainLoop.java
@@ -13,36 +13,13 @@ public class MainLoop{
 
   public static void main(String [ ] args)
   {
-	  // initialize the commandLnInterpreter
-	  // will execute the correct command based off the arguments from the command line
+    // initialize the commandLnInterpreter
+    // will execute the correct command based off the arguments from the command line
     CommandLnInterpreter comLn = new CommandLnInterpreter(args);
     FunctionsFromFile functions = comLn.getFunctionFromFile();
     ParseTree tree = new ParseTree(new ParseNode("", "", 0, 0, ParseNode.tType.WRONGO));
 
-    //Testing the getReturnRType method --------------
-    /*Method testMethod = functions.getFuncMethod("add", new ParseNode.rType[] {ParseNode.rType.INT, ParseNode.rType.INT});
-    ParseNode.rType rt = functions.getReturnRType(testMethod);
-    if (rt == ParseNode.rType.INT)
-      System.out.println("GOT INT");
-    else
-      System.out.println("didnt get INT");*//*
-    ParseNode.rType[] testrType = new ParseNode.rType[] {ParseNode.rType.INT};
-    Method testMethod = functions.getFuncMethod("add", testrType);
-    System.out.println(testMethod.getName());
-    for (Class<?> para : testMethod.getParameterTypes())
-      System.out.println(para);*/
-
-  //  ParseTree tree;
-  //  String expr = "(add 1 (add 3 4) )))";
-  /*  try {
-    } catch (ParseException e) {
-      System.out.println("Critical initialization error");
-    }*/
-    //int test = tree.checkBrackets(expr);
-    //ParseNode node = new ParseNode("hi",0,0,tType.VALUE);
-    //int test2 = node.returnType(expr);
     String expr;
-    System.out.println("Begin loop");
 
     comLn.printHelpText();
     Scanner reader = new Scanner(System.in);
@@ -69,28 +46,26 @@ public class MainLoop{
       }
       else if (expr.equals("q"))
         continue;
-      else{
+      else{ // Actual main body
         if (tree.checkBrackets(expr) == -1){
-        try {
-          // Generate a new parse tree from an expression
-          tree = new ParseTree(tree.genTree(expr, expr, 0));
-          ParseNode head = tree.getHead();
-
-          // Traverse the tree, assigning types to values and functions
-          tree.resolveTypes(head, functions);
-          // Compute the final result of the (now verified) tree
-          System.out.println(tree.computeTree(head, functions).toString());
-        } catch (ParseException e) {
-          System.out.println(e.getMessage());
-          if (verbose)
-            e.printStackTrace();
+          try {
+            // Generate a new parse tree from an expression
+            tree = new ParseTree(tree.genTree(expr, expr, 0));
+            ParseNode head = tree.getHead();
+  
+            // Traverse the tree, assigning types to values and functions
+            tree.resolveTypes(head, functions);
+            // Compute the final result of the (now verified) tree
+            System.out.println(tree.computeTree(head, functions).toString());
+          } catch (ParseException e) {
+            if (verbose) {
+              e.printStackTrace();
+            } else {
+              System.out.println(e.getMessage());
+            }
           }
         }
       }
-      // head.finishTree();
-      //head.assignMethod(functions);
-      //rType test2 = node.returnType(expr);
-      // parseTree.doEverything(expr);
     } while(!expr.equals("q"));
 
     System.out.println ( "bye." );
